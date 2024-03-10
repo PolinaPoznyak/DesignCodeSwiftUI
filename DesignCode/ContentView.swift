97d8206997e84899764321ba931ae1f7fb722d3d
Polina Poznyak <polinappoznyak@gmail.com>
2024-03-10 10:13:53 +0300

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Polina Poznyak on 10.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Polina Poznyak on 10.03.24.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
